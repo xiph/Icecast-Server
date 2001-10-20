5a4f55d74c4f816e00b5c2e5fe2706c069f7466a
Jack Moffitt <jack@xiph.org>
2001-10-20 05:03:24 +0000

    Change long long to the more standard uint64_t.  This will need to be typedefed for some platforms.
    svn path=/trunk/timing/; revision=2190

--- src/timing/timing.c
+++ src/timing/timing.c
@@ -3,6 +3,7 @@
 */
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <unistd.h>
@@ -16,7 +17,7 @@
 /* 
  * Returns milliseconds no matter what. 
  */
-long long timing_get_time(void)
+uint64_t timing_get_time(void)
 {
 #ifdef _WIN32
 	return timeGetTime();
@@ -25,16 +26,16 @@ long long timing_get_time(void)
 
 	gettimeofday(&mtv, NULL);
 
-	return (long long)(mtv.tv_sec) * 1000 + (long long)(mtv.tv_usec) / 1000;
+	return (uint64_t)(mtv.tv_sec) * 1000 + (uint64_t)(mtv.tv_usec) / 1000;
 #endif
 }
 
-void timing_sleep(long long sleeptime)
+void timing_sleep(uint64_t sleeptime)
 {
 	struct timeval sleeper;
 
 	sleeper.tv_sec = 0;
 	sleeper.tv_usec = sleeptime * 1000;
 
-	select(0, NULL, NULL, NULL, &sleeper);
+	select(1, NULL, NULL, NULL, &sleeper);
 }
--- src/timing/timing.h
+++ src/timing/timing.h
@@ -1,7 +1,9 @@
 #ifndef __TIMING_H__
 #define __TIMING_H__
 
-long long timing_get_time(void);
-void timing_sleep(long long sleeptime);
+#include <stdint.h>
+
+uint64_t timing_get_time(void);
+void timing_sleep(uint64_t sleeptime);
 
 #endif  /* __TIMING_H__ */
