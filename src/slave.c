080ce9787cd6977ec20092bb7a15292195ef5a2a
Karl Heyes <karl@xiph.org>
2004-07-26 19:19:47 +0000

    if the slave thread fails to auth against the master then log a warning and drop back to waiting
    svn path=/icecast/branches/kh/icecast/; revision=7352

--- src/slave.c
+++ src/slave.c
@@ -438,6 +438,14 @@ static int update_from_master(ice_config_t *config)
         free(authheader);
         free(data);
 
+        sock_read_line(mastersock, buf, sizeof(buf));
+        if (strncmp (buf, "HTTP/1.0 200", 12) != 0)
+        {
+            sock_close (mastersock);
+            WARN0 ("Master rejected authentication");
+            break;
+        }
+
         while (sock_read_line(mastersock, buf, sizeof(buf)))
         {
             if (!strlen(buf))
