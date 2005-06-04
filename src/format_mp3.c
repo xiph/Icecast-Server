9619ac178ce2b4ee7362bcb3e0b8c0b00f611643
Karl Heyes <karl@xiph.org>
2005-06-04 13:22:47 +0000

    test fix for real/helix metadata bug. Make the initial metadata StreamTitle=''; so that playback isn't screwed up
    svn path=/icecast/branches/kh/icecast/; revision=9347

--- src/format_mp3.c
+++ src/format_mp3.c
@@ -101,9 +101,9 @@ int format_mp3_get_plugin (source_t *source, http_parser_t *parser)
 
     /* initial metadata needs to be blank for sending to clients and for
        comparing with new metadata */
-    meta = refbuf_new (2);
-    memcpy (meta->data, "\0\0", 2);
-    meta->len = 1;
+    meta = refbuf_new (17);
+    memcpy (meta->data, "\001StreamTitle=''", 17);
+    meta->len = 17;
     state->metadata = meta;
     state->interval = -1;
 
