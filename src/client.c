43d8cfdd73db7648c8067efad15808c68fd2f62e
Karl Heyes <karl@xiph.org>
2005-10-06 02:41:51 +0000

    fix bug #717. a race on source exit could cause memory corruption
    svn path=/icecast/trunk/icecast/; revision=10135

--- src/client.c
+++ src/client.c
@@ -85,6 +85,14 @@ void client_destroy(client_t *client)
     if (client == NULL)
         return;
 
+    /* release the buffer now, as the buffer could be on the source queue
+     * and may of disappeared after auth completes */
+    if (client->refbuf)
+    {
+        refbuf_release (client->refbuf);
+        client->refbuf = NULL;
+    }
+
     if (release_client (client))
         return;
 
@@ -103,10 +111,6 @@ void client_destroy(client_t *client)
     global.clients--;
     stats_event_args (NULL, "clients", "%d", global.clients);
     global_unlock ();
-
-    /* drop ref counts if need be */
-    if (client->refbuf)
-        refbuf_release (client->refbuf);
 
     /* we need to free client specific format data (if any) */
     if (client->free_client_data)
