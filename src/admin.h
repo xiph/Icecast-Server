237eb4f7705a0d9916a720395795dfb0a46a5abf
Philipp Schafft <lion@lion.leolix.org>
2014-11-21 15:37:50 +0000

    first part of patch to allow kh like admin stats with listener tags inside
    svn path=/icecast/trunk/icecast/; revision=19343

--- src/admin.h
+++ src/admin.h
@@ -18,6 +18,7 @@
 
 #include "refbuf.h"
 #include "client.h"
+#include "source.h"
 
 #define RAW         1
 #define TRANSFORMED 2
@@ -26,5 +27,7 @@
 void admin_handle_request(client_t *client, const char *uri);
 void admin_send_response(xmlDocPtr doc, client_t *client, 
         int response, const char *xslt_template);
+
+void admin_add_listeners_to_mount(source_t *source, xmlNodePtr parent);
 
 #endif  /* __ADMIN_H__ */
