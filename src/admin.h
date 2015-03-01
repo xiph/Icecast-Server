314aa9660d15f0454a6292dc5c77687508659689
Thomas B. Ruecker <thomas@ruecker.fi>
2015-03-01 15:39:47 +0000

    remove some spurious tabs and replace with spaces
    Fix variable alignment if necessary.

--- src/admin.h
+++ src/admin.h
@@ -28,9 +28,9 @@
 #define ADMINTYPE_HYBRID    (ADMINTYPE_GENERAL|ADMINTYPE_MOUNT)
 
 /* formats */
-#define RAW         		1
-#define TRANSFORMED 		2
-#define PLAINTEXT   		3
+#define RAW                 1
+#define TRANSFORMED         2
+#define PLAINTEXT           3
 
 /* special commands */
 #define ADMIN_COMMAND_ERROR (-1)
@@ -38,14 +38,14 @@
 
 void admin_handle_request(client_t *client, const char *uri);
 
-void admin_send_response(xmlDocPtr 	doc,
-						 client_t 	*client, 
-        				 int 		response,
-        				 const char *xslt_template);
+void admin_send_response(xmlDocPtr    doc,
+                         client_t    *client, 
+                         int          response,
+                         const char  *xslt_template);
 
-void admin_add_listeners_to_mount(source_t 			*source,
-								  xmlNodePtr 		parent,
-								  operation_mode 	mode);
+void admin_add_listeners_to_mount(source_t       *source,
+                                  xmlNodePtr      parent,
+                                  operation_mode  mode);
 
 xmlNodePtr admin_add_role_to_authentication(auth_t *auth, xmlNodePtr parent);
 
