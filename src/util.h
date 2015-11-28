d51b9afc49fb193f3b3782ccca0a6f9c96aa02e2
Philipp Schafft <lion@lion.leolix.org>
2015-11-28 10:51:50 +0000

    Cleanup: unify util.h a bit with libshout.

--- src/util.h
+++ src/util.h
@@ -1,14 +1,14 @@
 /* Icecast
  *
- * This program is distributed under the GNU General Public License, version 2.
- * A copy of this license is included with this source.
- *
- * Copyright 2000-2004, Jack Moffitt <jack@xiph.org, 
+ *  Copyright 2000-2004 Jack Moffitt <jack@xiph.org, 
  *                      Michael Smith <msmith@xiph.org>,
  *                      oddsock <oddsock@xiph.org>,
  *                      Karl Heyes <karl@xiph.org>
  *                      and others (see AUTHORS for details).
- * Copyright 2012-2014, Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>,
+ *  Copyright 2012-2015 Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>
+ *
+ *  This program is distributed under the GNU General Public License, version 2.
+ *  A copy of this license is included with this source.
  */
 
 #ifndef __UTIL_H__
