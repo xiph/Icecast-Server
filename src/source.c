xiph/Icecast-Server

commit 461a537561580cb32dac327bae73f7e670188bd4

More detailed logging Add source IP adress to startup and source exit logging, see #2016 Add mountpoint to some log lines, see #1388
svn path=/icecast/trunk/icecast/; revision=19250

src/source.c
@@ -792,7 +792,7 @@ void source_main (source_t *source)
                 avl_delete(source->pending_tree, (void *)client, _free_client);
 
                 LOG_INFO("Client deleted, exceeding maximum listeners for this "
-                        "mountpoint.");
+                        "mountpoint (%s).", source->mount);
                 continue;
             }
             
@@ -800,7 +800,7 @@ void source_main (source_t *source)
             avl_insert(source->client_tree, client_node->key);
 
             source->listeners++;
-            LOG_DEBUG("Client added");
+            LOG_DEBUG("Client added for mountpoint (%s)", source->mount);
             stats_event_inc(source->mount, "connections");
 
             client_node = avl_get_next(client_node);
@@ -869,7 +869,7 @@ static void source_shutdown (source_t *source)
     mount_proxy *mountinfo;
 
     source->running = 0;
-    LOG_INFO("Source \"%s\" exiting", source->mount);
+    LOG_INFO("Source from %s at \"%s\" exiting", source->con->ip, source->mount);
 
     mountinfo = config_find_mount (config_get_config(), source->mount, MOUNT_TYPE_NORMAL);
     if (mountinfo)

src/connection.c
@@ -1036,7 +1036,8 @@ int connection_check_pass (http_parser_t *parser, const char *user, const char *
 /* only called for native icecast source clients */
 static void _handle_source_request (client_t *client, const char *uri)
 {
-    LOG_INFO("Source logging in at mountpoint \"%s\"", uri);
+    LOG_INFO("Source logging in at mountpoint \"%s\" from %s",
+        uri, client->con->ip);
 
     if (uri[0] != '/')
     {

commit fa1915c46d2c7e3c14f4183ab5ddaca4188fc0ee

Fix: Fixed regression introduced by r19250.
This fixes the regression introduced by r19250 (461a537561580cb32dac327bae73f7e670188bd4).
The fix checks if the source client is actually known before printing it's IP-Address.

See also: #2016

src/source.c
@@ -878,7 +878,11 @@ static void source_shutdown (source_t *source)
     mount_proxy *mountinfo;
 
     source->running = 0;
-    ICECAST_LOG_INFO("Source from %s at \"%s\" exiting", source->con->ip, source->mount);
+    if (source->con && source->con->ip) {
+        ICECAST_LOG_INFO("Source from %s at \"%s\" exiting", source->con->ip, source->mount);
+    } else {
+        ICECAST_LOG_INFO("Source at \"%s\" exiting", source->mount);
+    }
 
     mountinfo = config_find_mount (config_get_config(), source->mount, MOUNT_TYPE_NORMAL);
     if (mountinfo)