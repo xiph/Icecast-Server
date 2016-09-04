0d7f81b75a5c1d7c8cdcb29d0c475fad97444f28
Philipp Schafft <lion@lion.leolix.org>
2016-09-04 13:53:22 +0000

    Feature: Added support to extract metadata from Opus streams.
    This adds support to read metadata from Ogg/Opus streams:
     * Original sample rate from OpusHead,
     * number of channels from OpusHead,
     * Tags from OpusTags.

--- src/format_opus.c
+++ src/format_opus.c
@@ -23,6 +23,7 @@
 typedef struct source_tag source_t;
 
 #include "format_opus.h"
+#include "stats.h"
 #include "refbuf.h"
 #include "client.h"
 
@@ -31,10 +32,162 @@ typedef struct source_tag source_t;
 
 static void opus_codec_free (ogg_state_t *ogg_info, ogg_codec_t *codec)
 {
+    stats_event(ogg_info->mount, "audio_channels", NULL);
+    stats_event(ogg_info->mount, "audio_samplerate", NULL);
     ogg_stream_clear (&codec->os);
     free (codec);
 }
 
+static uint32_t __read_header_u32be_unaligned(const unsigned char *in)
+{
+    uint32_t ret = 0;
+    ret += in[3];
+    ret <<= 8;
+    ret += in[2];
+    ret <<= 8;
+    ret += in[1];
+    ret <<= 8;
+    ret += in[0];
+    return ret;
+}
+
+static void __handle_header_opushead(ogg_state_t *ogg_info, ogg_packet *packet)
+{
+    if (packet->bytes < 19) {
+        ICECAST_LOG_WARN("Bad Opus header: header too small, expected at least 19 byte, got %li", (long int)packet->bytes);
+        return; /* Invalid OpusHead */
+    }
+
+    ICECAST_LOG_DEBUG("Opus Header: len=%lu, data=%i,%i %i,%i %i,%i %i,%i %i,%i %i,%i %i,%i %i,%i %i,%i %i",
+                        (unsigned long int)packet->bytes,
+                        packet->packet[ 0], packet->packet[ 1], packet->packet[ 2], packet->packet[ 3],
+                        packet->packet[ 4], packet->packet[ 5], packet->packet[ 6], packet->packet[ 7],
+                        packet->packet[ 8], packet->packet[ 9], packet->packet[10], packet->packet[11],
+                        packet->packet[12], packet->packet[13], packet->packet[14], packet->packet[15],
+                        packet->packet[16], packet->packet[17], packet->packet[18]
+                     );
+
+
+    if ((packet->packet[8] & 0xF0) != 0) {
+        ICECAST_LOG_WARN("Bad Opus header: bad header version, expected major 0, got %i", (int)packet->packet[8]);
+        return; /* Invalid OpusHead Version */
+    }
+
+    stats_event_args(ogg_info->mount, "audio_channels", "%ld", (long int)packet->packet[9]);
+    stats_event_args(ogg_info->mount, "audio_samplerate", "%ld", (long int)__read_header_u32be_unaligned(packet->packet+12));
+}
+
+static void __handle_header_opustags(ogg_state_t *ogg_info, ogg_packet *packet, format_plugin_t *plugin) 
+{
+    size_t comments;
+    size_t next;
+    size_t left = packet->bytes;
+    size_t buflen = 0;
+    char *buf = NULL;
+    char *buf_new;
+    const void *p = packet->packet;
+
+    if (packet->bytes < 16) {
+        ICECAST_LOG_WARN("Bad Opus header: header too small, expected at least 16 byte, got %li", (long int)packet->bytes);
+        return; /* Invalid OpusHead */
+    }
+
+    /* Skip header "OpusTags" */
+    p += 8;
+    left -= 8;
+
+    /* Now the vendor string follows. We just skip it. */
+    next = __read_header_u32be_unaligned(p);
+    p += 4;
+    left -= 4;
+
+    if (left < (next + 4)) {
+        ICECAST_LOG_WARN("Bad Opus header: corrupted OpusTags header.");
+        return;
+    }
+    p += next;
+    left -= next;
+
+    /* Next is the comment counter. */
+    comments = __read_header_u32be_unaligned(p);
+    p += 4;
+    left -= 4;
+
+    /* Ok, next (comments) blocks follows, each composed of 4 byte length followed by the data */
+    if (left < (comments * 4)) {
+        ICECAST_LOG_WARN("Bad Opus header: corrupted OpusTags header.");
+        return;
+    }
+
+    vorbis_comment_clear(&plugin->vc);
+    vorbis_comment_init(&plugin->vc);
+
+    while (comments) {
+        next = __read_header_u32be_unaligned(p);
+        p += 4;
+        left -= 4;
+
+        if (left < next) {
+            if (buf)
+                free(buf);
+            vorbis_comment_clear(&plugin->vc);
+            vorbis_comment_init(&plugin->vc);
+            ICECAST_LOG_WARN("Bad Opus header: corrupted OpusTags header.");
+            return;
+        }
+
+        if ((next + 1) > buflen) {
+            buf_new = realloc(buf, next + 1);
+            if (buf_new) {
+                buf = buf_new;
+                buflen = next + 1;
+            }
+        }
+
+        if (buflen >= (next + 1)) {
+            memcpy(buf, p, next);
+            buf[next] = 0;
+            vorbis_comment_add(&plugin->vc, buf);
+        }
+
+        p += next;
+        left -= next;
+
+        comments--;
+        if (comments && left < 4) {
+            if (buf)
+                free(buf);
+            vorbis_comment_clear(&plugin->vc);
+            vorbis_comment_init(&plugin->vc);
+            ICECAST_LOG_WARN("Bad Opus header: corrupted OpusTags header.");
+            return;
+        }
+    }
+
+    if (buf)
+        free(buf);
+
+    ogg_info->log_metadata = 1;
+}
+
+static void __handle_header(ogg_state_t *ogg_info,
+        ogg_codec_t *codec, ogg_packet *packet, format_plugin_t *plugin)
+{
+    ICECAST_LOG_DEBUG("Got Opus header");
+    if (packet->bytes < 8) {
+        ICECAST_LOG_DEBUG("Not a real header, less than 8 bytes in size.");
+        return; /* packet is not a header */
+    }
+
+    if (strncmp((const char*)packet->packet, "OpusHead", 8) == 0) {
+        __handle_header_opushead(ogg_info, packet);
+    } else if (strncmp((const char*)packet->packet, "OpusTags", 8) == 0) {
+        __handle_header_opustags(ogg_info, packet, plugin);
+    } else {
+        ICECAST_LOG_DEBUG("Unknown header or data.");
+        return; /* Unknown header or data */
+    }
+}
 
 static refbuf_t *process_opus_page (ogg_state_t *ogg_info,
         ogg_codec_t *codec, ogg_page *page, format_plugin_t *plugin)
@@ -50,6 +203,7 @@ static refbuf_t *process_opus_page (ogg_state_t *ogg_info,
         {
            /* first time around (normal case) yields comments */
            codec->headers++;
+            __handle_header(ogg_info, codec, &packet, plugin);
         }
         /* add header page to associated list */
         format_ogg_attach_header (ogg_info, page);
@@ -78,9 +232,11 @@ ogg_codec_t *initial_opus_page (format_plugin_t *plugin, ogg_page *page)
         free (codec);
         return NULL;
     }
+    __handle_header(ogg_info, codec, &packet, plugin);
     ICECAST_LOG_INFO("seen initial opus header");
     codec->process_page = process_opus_page;
     codec->codec_free = opus_codec_free;
+    codec->name = "Opus";
     codec->headers = 1;
     format_ogg_attach_header (ogg_info, page);
     return codec;
