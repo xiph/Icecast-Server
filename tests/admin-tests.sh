f9c50e841db0481c1f2ad4b0a91a47f83ee596a0
Marvin Scholz <epirat07@gmail.com>
2017-05-10 09:40:42 +0200

    First try at tests with autotools stuff

--- tests/admin-tests.sh
+++ tests/admin-tests.sh
@@ -4,7 +4,7 @@ testdir=$(dirname "$0")
 test -n "$testdir" && cd "$testdir" || exit
 
 ICECAST_BASE_URL="http://localhost:8000/"
-
+counter=1
 RETURN=0
 MOUNT_LISTENER_AUTH="foo.ogg"
 MOUNT_SOURCE_AUTH="test.ogg"
@@ -17,81 +17,104 @@ L_USER="foo"
 L_PASS="bar"
 L_AUTH="$L_USER:$L_PASS"
 
-echo "Starting Icecast"
+echo "# Starting Icecast"
 ../src/icecast -c ./icecast.xml 2> /dev/null &
 ICECAST_PID=$!
 sleep 5
 
-echo "Starting Source client on /$MOUNT_LISTENER_AUTH"
+echo "# Starting Source client on /$MOUNT_LISTENER_AUTH"
 ffmpeg -loglevel panic -re -f lavfi -i "sine=frequency=1000" -content_type application/ogg "icecast://$AUTH_SOURCE@127.0.0.1:8000/$MOUNT_LISTENER_AUTH" &
 SOURCE1_PID=$!
 
-echo "Starting Source client on /$MOUNT_SOURCE_AUTH"
+echo "# Starting Source client on /$MOUNT_SOURCE_AUTH"
 ffmpeg -loglevel panic -re -f lavfi -i "sine=frequency=1000" -content_type application/ogg "icecast://$AUTH_MOUNT@127.0.0.1:8000/$MOUNT_SOURCE_AUTH" &
 SOURCE2_PID=$!
 
 function test_endpoint {
-    echo " CURL $1"
+    echo "# CURL $1"
     if test "x$3" == "x"; then
         res=$(curl -m 5 -o /dev/null -D - "$ICECAST_BASE_URL$1" 2>/dev/null | head -n 1 | cut -d$' ' -f2- | tr -d '\r\n')
     else
         res=$(curl -m 5 -u "$3" -o /dev/null -D - "$ICECAST_BASE_URL$1" 2>/dev/null | head -n 1 | cut -d$' ' -f2- | tr -d '\r\n')
     fi
     if test "$(echo "$res" | cut -d$' ' -f1)" -eq "$2"; then
-        printf "   \e[92mOK\e[39m"
-        echo " [$res]"
+        #printf "   \e[92mOK\e[39m"
+        #echo " [$res]"
+        echo "ok $counter - foo"
     else
-        printf "   \e[91mFAIL\e[39m"
-        echo " [$res] Expected: $2"
+        #printf "   \e[91mFAIL\e[39m"
+        #echo " [$res] Expected: $2"
+        echo "not ok $counter - foo"
         RETURN=1
     fi
+    ((counter++))
 }
 
-echo
-echo "Testing admin/manageauth endpoint"
+function test_sourcing {
+    echo "# CURL $1"
+    if test "x$3" == "x"; then
+        res=$(curl -X PUT -m 5 -d /dev/random -o /dev/null -D - "$ICECAST_BASE_URL$1" 2>/dev/null | head -n 1 | cut -d$' ' -f2- | tr -d '\r\n')
+    else
+        res=$(dd if=/dev/urandom bs=8 count=32 2>/dev/null | curl -X PUT -m 5 -H "Content-Type: application/octet-stream" --data-binary "@-" -v -u "$3" -o /dev/null -D - "$ICECAST_BASE_URL$1" 2>curl-log.txt | head -n 1 | cut -d$' ' -f2- | tr -d '\r\n')
+    fi
+    if test "$(echo "$res" | cut -d$' ' -f1)" -eq "$2"; then
+        #printf "   \e[92mOK\e[39m"
+        #echo " [$res]"
+        echo "ok $counter - foo"
+    else
+        #printf "   \e[91mFAIL\e[39m"
+        #echo " [$res] Expected: $2"
+        echo "not ok $counter - foo"
+        RETURN=1
+    fi
+    ((counter++))
+}
+
+echo "#"
+echo "# Testing admin/manageauth endpoint"
 test_endpoint "admin/manageauth?id=4&username=$L_USER&password=$L_PASS&action=add"                              401
 test_endpoint "admin/manageauth?id=99&username=$L_USER&password=$L_PASS&action=add"                             401
 test_endpoint "admin/manageauth?id=99&username=$L_USER&password=$L_PASS&action=add"                             404 "$AUTH_ADMIN"
 test_endpoint "admin/manageauth?id=4&username=$L_USER&password=$L_PASS&action=add"                              200 "$AUTH_ADMIN"
 
-echo
-echo "Testing admin/buildm3u endpoint"
+echo "#"
+echo "# Testing admin/buildm3u endpoint"
 test_endpoint "admin/buildm3u?username=$L_USER&password=$L_PASS&mount=%2F$MOUNT_LISTENER_AUTH"                  200
 test_endpoint "admin/buildm3u?username=something&password=foo&mount=%2F$MOUNT_LISTENER_AUTH"                    200
 
-echo
-echo "Testing admin/stats endpoint"
+echo "#"
+echo "# Testing admin/stats endpoint"
 test_endpoint "admin/stats"                                                                                     401
 test_endpoint "admin/stats"                                                                                     401 "$AUTH_SOURCE"
 test_endpoint "admin/stats"                                                                                     401 "$L_AUTH"
 test_endpoint "admin/stats"                                                                                     200 "$AUTH_ADMIN"
 
-echo
-echo "Testing admin/listclients endpoint"
+echo "#"
+echo "# Testing admin/listclients endpoint"
 test_endpoint "admin/listclients"                                                                               401
 test_endpoint "admin/listclients"                                                                               401 "$AUTH_SOURCE"
 test_endpoint "admin/listclients"                                                                               401 "$L_AUTH"
 test_endpoint "admin/listclients"                                                                               400 "$AUTH_ADMIN"
 test_endpoint "admin/listclients?mount=%2F$MOUNT_LISTENER_AUTH"                                                 200 "$AUTH_ADMIN"
 
-echo
-echo "Testing admin/moveclients endpoint"
+echo "#"
+echo "# Testing admin/moveclients endpoint"
 test_endpoint "admin/moveclients"                                                                               401
 test_endpoint "admin/moveclients"                                                                               401 "$AUTH_SOURCE"
 test_endpoint "admin/moveclients"                                                                               401 "$L_AUTH"
 test_endpoint "admin/moveclients"                                                                               400 "$AUTH_ADMIN"
 test_endpoint "admin/moveclients?mount=%2F$MOUNT_LISTENER_AUTH"                                                 200 "$AUTH_ADMIN"
 
-echo
-echo "Testing admin/updatemetadata endpoint"
+echo "#"
+echo "# Testing admin/updatemetadata endpoint"
 test_endpoint "admin/updatemetadata"                                                                            401
 test_endpoint "admin/updatemetadata"                                                                            401 "$AUTH_SOURCE"
 test_endpoint "admin/updatemetadata"                                                                            401 "$L_AUTH"
 test_endpoint "admin/updatemetadata"                                                                            400 "$AUTH_ADMIN"
 test_endpoint "admin/updatemetadata?mount=%2F$MOUNT_LISTENER_AUTH"                                              200 "$AUTH_ADMIN"
 
-echo
-echo "Testing admin/metadata endpoint"
+echo "#"
+echo "# Testing admin/metadata endpoint"
 test_endpoint "admin/metadata"                                                                                  401
 test_endpoint "admin/metadata"                                                                                  401 "$AUTH_SOURCE"
 test_endpoint "admin/metadata"                                                                                  401 "$L_AUTH"
@@ -102,49 +125,67 @@ test_endpoint "admin/metadata?mount=%2F$MOUNT_SOURCE_AUTH&mode=updinfo&charset=U
 test_endpoint "admin/metadata?mount=%2F$MOUNT_SOURCE_AUTH&mode=updinfo&charset=UTF-8&song=Test2"                200 "$AUTH_ADMIN"
 test_endpoint "admin/metadata?mount=%2F$MOUNT_SOURCE_AUTH&mode=updinfo&charset=UTF-8&song=Test3"                200 "$AUTH_MOUNT"
 
-echo
-echo "Testing admin/listmounts endpoint"
+echo "#"
+echo "# Testing admin/listmounts endpoint"
 test_endpoint "admin/listmounts"                                                                                  401
 test_endpoint "admin/listmounts"                                                                                  401 "$AUTH_SOURCE"
 test_endpoint "admin/listmounts"                                                                                  401 "$L_AUTH"
 test_endpoint "admin/listmounts"                                                                                  200 "$AUTH_ADMIN"
 test_endpoint "admin/listmounts?mount=%2F$MOUNT_LISTENER_AUTH"                                                    400 "$AUTH_ADMIN"
 
-echo
-echo "Testing mountpoint which requires auth"
+echo "#"
+echo "# Testing mountpoint which requires auth"
 test_endpoint "$MOUNT_LISTENER_AUTH"                                                                              401
 test_endpoint "$MOUNT_LISTENER_AUTH"                                                                              401 "$AUTH_SOURCE"
 test_endpoint "$MOUNT_LISTENER_AUTH"                                                                              200 "$L_AUTH"
 test_endpoint "$MOUNT_LISTENER_AUTH"                                                                              401 "$AUTH_ADMIN"
 
-echo
-echo "Testing mountpoint which doesn't require auth"
+echo "#"
+echo "# Testing mountpoint which doesn't require auth"
 test_endpoint "$MOUNT_SOURCE_AUTH"                                                                                200
 test_endpoint "$MOUNT_SOURCE_AUTH"                                                                                200 "$AUTH_SOURCE"
 test_endpoint "$MOUNT_SOURCE_AUTH"                                                                                200 "$L_AUTH"
 test_endpoint "$MOUNT_SOURCE_AUTH"                                                                                200 "$AUTH_ADMIN"
 
-echo
-echo "Testing admin/killsource endpoint"
+echo "#"
+echo "# Testing admin/killsource endpoint"
 test_endpoint "admin/killsource"                                                                                  401
 test_endpoint "admin/killsource"                                                                                  401 "$AUTH_SOURCE"
 test_endpoint "admin/killsource"                                                                                  401 "$L_AUTH"
 test_endpoint "admin/killsource"                                                                                  400 "$AUTH_ADMIN"
 test_endpoint "admin/killsource?mount=%2F$MOUNT_LISTENER_AUTH"                                                    200 "$AUTH_ADMIN"
 
-echo
-echo "All tests done"
-echo
+echo "#"
+echo "# Testing on-connect handling with probing"
+test_sourcing "test-on-connect.ogg"                                                                               401
+if [ -f "on-connect-success" ]; then
+    echo "#FAIL: on-connect triggered even though it should not!"
+    rm "on-connect-success"
+fi
+test_sourcing "test-on-connect.ogg"                                                                               200 "$AUTH_SOURCE"
+if [ -f "on-connect-success" ]; then
+    echo "#OK: on-connect triggered!"
+    rm "on-connect-success"
+else
+    echo "#FAIL: on-connect not triggered!"
+fi
+
+echo "#"
+echo "#All tests done, cleanup..."
+echo "#"
+
+rm "myauth"
 
 if kill $SOURCE1_PID; then
-    echo "Terminated SOURCE1"
+    echo "#Terminated SOURCE1"
 fi
 if kill $SOURCE2_PID; then
-    echo "Terminated SOURCE2"
+    echo "#Terminated SOURCE2"
 fi
 
 if kill $ICECAST_PID; then
-    echo "Terminated Icecast"
+    echo "#Terminated Icecast"
 fi
 
+echo "1..(($counter-1))" # Number of tests to be executed.
 exit $RETURN
