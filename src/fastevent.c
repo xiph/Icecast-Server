8cf3da1a9215884793ffbbf44ecbf6a1884d7299
Philipp Schafft <lion@lion.leolix.org>
2018-06-06 12:11:02 +0000

    Update: Added still stub files for fast events.

---
+++ src/fastevent.c
@@ -0,0 +1,21 @@
+/* Icecast
+ *
+ * This program is distributed under the GNU General Public License, version 2.
+ * A copy of this license is included with this source.
+ *
+ * Copyright 2018,      Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>,
+ */
+
+/**
+ * Special fast event functions
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "fastevent.h"
+
+#include "logging.h"
+#define CATMODULE "fastevent"
+
---
+++ src/fastevent.h
@@ -0,0 +1,12 @@
+/* Icecast
+ *
+ * This program is distributed under the GNU General Public License, version 2.
+ * A copy of this license is included with this source.
+ *
+ * Copyright 2018,      Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>,
+ */
+
+#ifndef __FASTEVENT_H__
+#define __FASTEVENT_H__
+
+#endif
