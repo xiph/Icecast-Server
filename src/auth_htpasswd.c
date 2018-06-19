xiph/Icecast-Server

commit 4828c07d6b938780b4c4113f887725f811a75c9b

Fix: Fixed segfault in htpasswd auth if no filename is set

src/auth_htpasswd.c
@@ -191,6 +191,11 @@ static auth_result htpasswd_auth (auth_client *auth_user)
     }
     htpasswd_recheckfile (htpasswd);
 
+    if (htpasswd->users) {
+        ICECAST_LOG_ERROR("No user list.");
+        return AUTH_NOMATCH;
+    }
+
     thread_rwlock_rlock (&htpasswd->file_rwlock);
     entry.name = client->username;
     if (avl_get_by_key (htpasswd->users, &entry, &result) == 0) {
@@ -257,7 +262,17 @@ static auth_result htpasswd_adduser (auth_t *auth, const char *username, const c
     htpasswd_user entry;
     void *result;
 
+    if (!state->filename) {
+        ICECAST_LOG_ERROR("No filename given in options for authenticator.");
+        return AUTH_FAILED;
+    }
+
     htpasswd_recheckfile (state);
+
+    if (state->users) {
+        ICECAST_LOG_ERROR("No user list.");
+        return AUTH_FAILED;
+    }
 
     thread_rwlock_wlock (&state->file_rwlock);
 
@@ -301,6 +316,17 @@ static auth_result htpasswd_deleteuser(auth_t *auth, const char *username)
     struct stat file_info;
 
     state = auth->state;
+
+    if (!state->filename) {
+        ICECAST_LOG_ERROR("No filename given in options for authenticator.");
+        return AUTH_FAILED;
+    }
+
+    if (state->users) {
+        ICECAST_LOG_ERROR("No user list.");
+        return AUTH_FAILED;
+    }
+
     thread_rwlock_wlock (&state->file_rwlock);
     passwdfile = fopen(state->filename, "rb");
 
@@ -383,7 +409,17 @@ static auth_result htpasswd_userlist(auth_t *auth, xmlNodePtr srcnode)
 
     state = auth->state;
 
+    if (!state->filename) {
+        ICECAST_LOG_ERROR("No filename given in options for authenticator.");
+        return AUTH_FAILED;
+    }
+
     htpasswd_recheckfile(state);
+
+    if (state->users) {
+        ICECAST_LOG_ERROR("No user list.");
+        return AUTH_FAILED;
+    }
 
     thread_rwlock_rlock(&state->file_rwlock);
     node = avl_get_first(state->users);

commit ea42a12efb33edee8703c97017139ce749b9e954

Fix: Fixed regression in htpasswd auth detecting no user list.
This fixed a regression caused by 4828c07d6b938780b4c4113f887725f811a75c9b.
The patch was also applied into release-2.4.4 as c5201770b6cc1ae9db9f59b91401f38be4b2123e.
However the patch for release-2.4.4 was correct. So no regression there.

src/auth_htpasswd.c
@@ -171,6 +171,8 @@ static void htpasswd_recheckfile(htpasswd_auth_state *htpasswd)
         avl_tree_free (htpasswd->users, _free_user);
     htpasswd->users = new_users;
     thread_rwlock_unlock (&htpasswd->file_rwlock);
+
+    ICECAST_LOG_ERROR("xxx");
 }
 
 
@@ -191,7 +193,7 @@ static auth_result htpasswd_auth (auth_client *auth_user)
     }
     htpasswd_recheckfile (htpasswd);
 
-    if (htpasswd->users) {
+    if (htpasswd->users == NULL) {
         ICECAST_LOG_ERROR("No user list.");
         return AUTH_NOMATCH;
     }
@@ -262,14 +264,14 @@ static auth_result htpasswd_adduser (auth_t *auth, const char *username, const c
     htpasswd_user entry;
     void *result;
 
-    if (!state->filename) {
+    if (state->filename == NULL) {
         ICECAST_LOG_ERROR("No filename given in options for authenticator.");
         return AUTH_FAILED;
     }
 
     htpasswd_recheckfile (state);
 
-    if (state->users) {
+    if (state->users == NULL) {
         ICECAST_LOG_ERROR("No user list.");
         return AUTH_FAILED;
     }
@@ -322,7 +324,7 @@ static auth_result htpasswd_deleteuser(auth_t *auth, const char *username)
         return AUTH_FAILED;
     }
 
-    if (state->users) {
+    if (state->users == NULL) {
         ICECAST_LOG_ERROR("No user list.");
         return AUTH_FAILED;
     }
@@ -416,7 +418,7 @@ static auth_result htpasswd_userlist(auth_t *auth, xmlNodePtr srcnode)
 
     htpasswd_recheckfile(state);
 
-    if (state->users) {
+    if (state->users == NULL) {
         ICECAST_LOG_ERROR("No user list.");
         return AUTH_FAILED;
     }