f8e30d46a4b17aed65b0c6ce3ab7e9d8cfe70011
Marvin Scholz <epirat07@gmail.com>
2018-08-07 19:13:39 +0200

    Cleanup: Comments and output in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# Run this to set up the build system: configure, makefiles, etc.
+# Run this to generate the configure script
 set -e
 
 srcdir=`dirname $0`
 test -n "$srcdir" && cd "$srcdir"
 
-echo "Updating build configuration files for Icecast, please wait...."
+echo "Updating build configuration files for Icecast, please wait..."
 
 autoreconf -isf
