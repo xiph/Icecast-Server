9a3782173cc687bb3876c233de9c9c9d09db3718
Marvin Scholz <epirat07@gmail.com>
2018-08-07 19:29:33 +0200

    Fix: Detect missing submodules in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,17 @@ set -e
 srcdir=`dirname $0`
 test -n "$srcdir" && cd "$srcdir"
 
+if ! {
+# Test for any file in the m4 submodule
+  test -e "$srcdir/m4/ax_check_compile_flag.m4" &&
+# Test for any file in the common submodule
+  test -e "$srcdir/src/common/log/log.c"; };
+then
+    echo "ERROR: Submodules missing! Ensure you git cloned with '--recursive' or run:"
+    echo "         git submodule update --init"
+    exit 1
+fi
+
 echo "Updating build configuration files for Icecast, please wait..."
 
 autoreconf -isf
