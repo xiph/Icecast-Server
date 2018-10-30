3da438a8da685e302fd069062a8d770256fd6dd0
Thomas B. Ruecker <thomas@ruecker.fi>
2018-10-30 13:53:53 +0000

    Win32 clean up
    * Removed all files related to the removed Windows UI
    * Added files needed by NSIS
    * Added batch file used to start icecast on Windows

---
+++ win32/icecast.bat
@@ -0,0 +1,12 @@
+@echo OFF
+echo.
+echo.
+echo|set /p=Starting
+.\bin\icecast.exe -v
+echo Using config "icecast.xml" from installation directory ...
+echo.
+echo Please open http://localhost:8000 in your web browser to see the web interface.
+echo.
+echo.
+echo Leave this window open to keep Icecast running and, if necessary, minimize it.
+.\bin\icecast.exe -c .\etc\icecast.xml
