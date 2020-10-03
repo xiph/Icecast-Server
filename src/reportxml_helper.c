44af32bac534eacc7fc7778a63094d131b49fb2f
Philipp Schafft <lion@lion.leolix.org>
2020-10-03 08:51:22 +0000

    Feature: Added stub src/reportxml_helper.[ch]

---
+++ src/reportxml_helper.c
@@ -0,0 +1,21 @@
+/* Icecast
+ *
+ * This program is distributed under the GNU General Public License, version 2.
+ * A copy of this license is included with this source.
+ *
+ * Copyright 2018-2020, Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>,
+ */
+
+/**
+ * This file contains helper functions for report XML document parsing, manipulation, and rendering.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "reportxml_helper.h"
+
+#include "logging.h"
+#define CATMODULE "reportxml-helper"
+
