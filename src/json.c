261ee12c04a922fe53addaa6aed0aa87390a7eb8
Philipp Schafft <lion@lion.leolix.org>
2020-10-04 06:55:30 +0000

    Feature: Added stub src/json.[ch]

---
+++ src/json.c
@@ -0,0 +1,23 @@
+/* Icecast
+ *
+ * This program is distributed under the GNU General Public License, version 2.
+ * A copy of this license is included with this source.
+ *
+ * Copyright 2018-2020, Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>,
+ */
+
+/**
+ * This file contains functions for rendering JSON.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+
+#include "json.h"
+
+#include "logging.h"
+#define CATMODULE "json"
+
---
+++ src/json.h
@@ -0,0 +1,15 @@
+/* Icecast
+ *
+ * This program is distributed under the GNU General Public License, version 2.
+ * A copy of this license is included with this source.
+ *
+ * Copyright 2020,      Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>,
+ */
+
+/* This file contains functions for rendering JSON. */
+
+#ifndef __JSON_H__
+#define __JSON_H__
+
+
+#endif
