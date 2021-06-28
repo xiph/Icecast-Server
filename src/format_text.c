d6be48e207751c2058be5b27cb0bf7c8ee89e69a
Philipp Schafft <lion@lion.leolix.org>
2021-06-28 18:03:51 +0000

    Update: Added stubs format_text.[ch] for text streaming
    See: #2084

---
+++ src/format_text.c
@@ -0,0 +1,20 @@
+/* Icecast
+ *
+ * This program is distributed under the GNU General Public License,
+ * version 2. A copy of this license is included with this source.
+ * At your option, this specific source file can also be distributed
+ * under the GNU GPL version 3.
+ *
+ * Copyright 2021,      Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>,
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "format_text.h"
+
+#define CATMODULE "format-text"
+
+#include "logging.h"
+
