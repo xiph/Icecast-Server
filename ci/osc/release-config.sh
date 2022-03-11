0c8a42abb1861b30ebcfd168c29304b539b08098
Stephan Jauernick <info@stephan-jauernick.de>
2022-03-11 21:41:02 +0100

    change release version

--- ci/osc/release-config.sh
+++ ci/osc/release-config.sh
@@ -2,5 +2,5 @@ export OBS_BASE=home:stephan48:branches:multimedia:xiph:beta-release
 export ICECAST_PROJECT=icecast
 export W32_ICECAST_PROJECT=mingw32-icecast
 export W32_ICECAST_INSTALLER_PROJECT=mingw32-icecast-installer
-export ICECAST_VERSION=$CI_COMMIT_TAG
+export ICECAST_VERSION=2.4.99.2
 export ICECAST_CI_VERSION=$ICECAST_VERSION
