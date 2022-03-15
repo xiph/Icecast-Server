b23ef22eaf783360ec70b98f6e3c5dbfc89a0a90
Philipp Schafft <lion@lion.leolix.org>
2022-03-15 23:55:45 +0000

    Update: Added stub for Xiph metadata (vorbis comments, ...)

---
+++ src/metadata_xiph.h
@@ -0,0 +1,12 @@
+/* Icecast
+ *
+ * This program is distributed under the GNU General Public License, version 2.
+ * A copy of this license is included with this source.
+ *
+ * Copyright 2022,      Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>
+ */
+
+#ifndef __METADATA_XIPH_H__
+#define __METADATA_XIPH_H__
+
+#endif
