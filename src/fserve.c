e3fe175bfc0d994f806f957ba8e78e335c45be5f
Philipp Schafft <lion@lion.leolix.org>
2022-03-20 15:58:45 +0000

    Update: Renamed thread as it is too long on Linux

--- src/fserve.c
+++ src/fserve.c
@@ -629,7 +629,7 @@ static void fserve_add_pending (fserve_t *fclient)
     {
         run_fserv = 1;
         ICECAST_LOG_DEBUG("fserve handler waking up");
-        thread_create("File Serving Thread", fserv_thread_function, NULL, THREAD_DETACHED);
+        thread_create("File Serving", fserv_thread_function, NULL, THREAD_DETACHED);
     }
     thread_spin_unlock (&pending_lock);
 }
