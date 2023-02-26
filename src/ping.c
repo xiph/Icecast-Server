e3f9f5b078e3f1fc33b2f3344d2c3255f979737e
Philipp Schafft <phschafft@de.loewenfelsen.net>
2023-02-26 21:30:37 +0000

    Feature: Added stub for ping requests

---
+++ src/ping.c
@@ -0,0 +1,14 @@
+/* Icecast
+ *
+ *  Copyright 2023      Philipp "ph3-der-loewe" Schafft <lion@lion.leolix.org>
+ *
+ *  This program is distributed under the GNU General Public License, version 2.
+ *  A copy of this license is included with this source.
+ */
+
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "ping.h"
