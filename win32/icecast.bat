cb60eb95feae02f20a665b753397f5c34f163e4b
Stephan Jauernick <info@stephan-jauernick.de>
2025-12-07 14:14:23 +0100

    Feature: Allow local config file to override default config in icecast.bat

--- win32/icecast.bat
+++ win32/icecast.bat
@@ -3,10 +3,19 @@ echo.
 echo.
 echo|set /p=Starting
 .\bin\icecast.exe -v
-echo Using config "icecast.xml" from installation directory ...
+
+set CONFIG=icecast.xml
+set CONFIG_LOCAL=icecast.local.xml
+
+IF EXIST %CONFIG_LOCAL% (
+  set CONFIG=%CONFIG_LOCAL%
+)
+
+
+echo Using config "%CONFIG%" from installation directory ...
 echo.
-echo Please open http://localhost:8000/ in your web browser to see the web interface.
+echo Please open http://localhost:8000/ (if you have not changed default ports) in your web browser to see the web interface.
 echo.
 echo.
 echo Leave this window open to keep Icecast running and, if necessary, minimize it.
-.\bin\icecast.exe -c icecast.xml
+.\bin\icecast.exe -c %CONFIG%
